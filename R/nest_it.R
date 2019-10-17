f40bab382feab81405b8dd67b05e5a45bf1a0c4e
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2019-10-17 00:02:13 -0500

    making function easier

--- R/nest_it.R
+++ R/nest_it.R
@@ -1,9 +1,14 @@
 #' @title Nest Dataframe
+#'
+#' Simple function to nest dataframes given a list of column names.
+#'
 #' @import dplyr
+#' @import tidyr
+#' @import rlang
 #' @export
 
 nest_it <- function(.data, ...){
-  group_var <- enquos(...)
-  df_group_by <- .data %>% dplyr::ungroup() %>% dplyr::group_by(!!! group_var) %>% tidyr::nest()
+  df_group_by <- tidyr::nest(dplyr::group_by(.data, ...))
   return(df_group_by)
 }
+
