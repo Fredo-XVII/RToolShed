1250bab2242c0fbea8b69fb484640cbd899ef2bb
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-05-22 22:47:43 -0500

    fix package info doc, added glue

--- R/RToolShed-package.R
+++ R/RToolShed-package.R
@@ -1,6 +1,6 @@
 #' @keywords internal
 #' @author Alfredo G Marquez
-#' @email alfredo.g.marquez@gmail.com
+#' @email alfredo.g.marquez at gmail.com
 #' @docType package
 #' @name RTooShed
 "_PACKAGE"
