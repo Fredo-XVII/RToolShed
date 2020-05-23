a6a216399152ab943c4056a5a50e8092281f3e73
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-05-22 23:25:50 -0500

    adding appveyor

---
+++ pkg_dev/Package_Development_Script.R
@@ -0,0 +1,103 @@
+# Package Development Script
+
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+library(usethis)
+library(devtools)
+library(roxygen2)
+library(testthat)
+library(purrr)
+
+# Create Package, versioning, and documentation
+#tmp <- file.path("FILEPATH", "TestContR")
+usethis::use_package_doc()
+usethis::use_travis()
+usethis::use_appveyor()
+
+# Package Documentation and Vignettes
+usethis::use_travis()
+usethis::use_mit_license("Alfredo G Marquez")
+usethis::use_readme_rmd()
+usethis::use_news_md(open = interactive())
+usethis::use_pkgdown()
+usethis::use_package_doc()
+usethis::use_vignette("to_postgres") #
+usethis::use_vignette("rm_db_name") #
+
+# Add Functions
+usethis::use_r("rm_db_name")
+usethis::use_r("to_postgres")
+usethis::use_r("pg_dbCreatePrimaryIndex")
+usethis::use_r("ts_exposed")
+usethis::use_r("nest_it")
+usethis::use_r("prep_multidplyr")
+usethis::use_r("write_df_to_hive")
+usethis::use_r("write_csv_to_hive")
+
+
+# Add Packages
+
+## Import functions
+usethis::use_roxygen_md()
+usethis::use_pipe()
+import_pkg_list <- c("RPostgres","stringr","dbplyr","dplyr","rlang","tidyr","askpass","ssh",
+                     "glue")
+purrr::map2(import_pkg_list, .y = "Imports", .f = usethis::use_package)
+
+## Suggests
+
+usethis::use_package( "roxygen2", type = "Suggests")
+
+usethis::use_package( "kableExtra", type = "Suggests")
+
+usethis::use_package( "remotes", type = "Suggests") #appveyor build
+
+
+# After adding roxygen2 params to function in R folder
+roxygen2::roxygenise()
+devtools::document()
+devtools::load_all()
+
+# Other Options
+usethis::use_tidy_versions() # adds versions to Description file - cool
+usethis::use_cc0_license("Alfredo G Marquez")
+
+usethis::use_namespace()
+usethis::use_testthat()
+
+# Update Version
+usethis::use_version()
+
+# Build Pkgdown
+pkgdown::build_site()
+
+
+# Buildignore: Add directory
+usethis::use_build_ignore("docs")
+usethis::use_build_ignore(".Rhistory")
+usethis::use_build_ignore("pkg_dev")
+
+
+# Spell Check
+
+usethis::use_spell_check()
+
+
+# Functions Roxygen format
+#' @title
+#'
+#' @description
+#'
+#' @details
+#'
+#' @param
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @export
