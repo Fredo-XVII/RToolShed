a305e77f58adf636c1d4d21c6b055a2abcf60f55
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2020-05-23 22:58:31 -0500

    add code

--- R/lag_it.R
+++ R/lag_it.R
@@ -12,3 +12,23 @@
 #' @examples
 #'
 #>' @export
+
+# Lags and Differences
+lag_it <- function(df,col,lags = 1, mutate_type = c("mutate","trans")) {
+  
+  # Build Lags and Lag Column Names
+  lags_num <- 1:lags
+  col_names <- paste(col, "lag", lags_num, sep = "_" )
+  
+  # Build functions based on lags
+  build_fx <- glue::glue("dplyr::lag(., { lags_num } )")
+  lag_functions <- setNames(build_fx, col_names)
+  
+  # Choose between mutate and transmutate
+  if (mutate_type[1] == "trans") {
+    out_df <- df %>% transmute_at(vars(!!col), funs_(lag_functions))
+  } else {
+    out_df <- df %>% mutate_at(vars(!!col), funs_(lag_functions))
+  }
+  return(out_df)
+}
