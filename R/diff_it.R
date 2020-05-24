a7aaf20e1e9050f0e9a6169446e3e7d19ec49458
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2020-05-23 22:56:47 -0500

    add code

--- R/diff_it.R
+++ R/diff_it.R
@@ -13,3 +13,23 @@
 #' @examples
 #'
 #>' @export
+
+diff_it <- function(df,col,lags = 1, diff = 1, mutate_type = c("mutate","trans")) {
+  
+  # Build Lags and Differences, and Column Names
+  lags_num <- 1:lags
+  diff_num <- 1:diff
+  col_names <- paste(col, "diff", diff_num, "lag", lags_num, sep = "_" )
+  
+  # Build functions based on lags
+  build_fx <- glue::glue("tsibble::difference(., lag = { lags_num }, difference = { diff })")
+  lag_functions <- setNames(build_fx, col_names)
+  
+  # Choose between mutate and transmutate  
+  if (mutate_type[1] == "trans") {
+    out_df <- df %>% transmute_at(vars(!!col), funs_(lag_functions)) 
+  } else {
+    out_df <- df %>% mutate_at(vars(!!col), funs_(lag_functions)) 
+  }
+  return(out_df)
+}
