276894f9c7651646042ccbbe91f3bf2fcfdff217
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-05-30 23:47:01 -0500

    rad2deg now prod, docs updated

--- R/rad2deg.R
+++ R/rad2deg.R
@@ -1,2 +1,15 @@
-# Convert radians to degree: d = r ⋅ 180 ∕ π
-rad2deg <- function(r) r*180/pi 
+#' @title  Convert radians to degree: d = r ⋅ 180 ∕ π
+#'
+#' @details Convert radians to degree: d = r ⋅ 180 ∕ π
+#'
+#' @param r numeric - radians, or output from a trigonmetric functions.
+#'
+#' @return A number in degrees that was converted from radians.
+#'
+#' @examples
+#' rads <- sin(4/5)
+#' rad2deg(rads)
+#'
+#' @export
+
+rad2deg <- function(r) r*180/pi
