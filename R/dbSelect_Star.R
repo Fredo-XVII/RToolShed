009efa9c317f378bb623c239629b54c0fb35dd7c
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-05-30 23:56:33 -0500

    ready for prod

--- R/dbSelect_Star.R
+++ R/dbSelect_Star.R
@@ -1,4 +1,23 @@
-## Function to get data
+#' @title Select Star
+#'
+#' This functions selects stars from a table given a connection.
+#'
+#' @param con A connection object
+#' @param db_tbl string - name of table, in schema.table format.s
+#'
+#' @return a dataframe/tibble with the results of the query.
+#'
+#' @import glue glue
+#'
+#' @examples
+#' \dontrun{
+#'
+#' db_con <- RPostgres::dbConnect()
+#' df <- dbSelect_Star(db_con, "schema.table")
+#' }
+#'
+#' @export
+
 dbSelect_Star <- function(con, db_tbl) {
   RODBC::sqlQuery(con,
                   glue::glue("
