446f92763e4057006ba02620bdb986b7e9360cbd
Alfredo G Marquez <alfredo.g.marquez@gmail.com>
2020-05-30 23:35:29 -0500

    set for prod

--- R/df_name_dot_csv.R
+++ R/df_name_dot_csv.R
@@ -1,4 +1,16 @@
-# df_name_dot_csv
+#' @title Dataframe to CSV
+#'
+#' @details This function takes a dataframe and extracts the name of the dataframe
+#' as text or string.  You can then use this string to name a csv file.
+#'
+#' @param x dataframe or tibble
+#'
+#' @return Returns the name of the dataframe as a text.
+#'
+#' @examples
+#' df <- as.data.frame(datasets::cars)
+#' df_name_dot_csv(df)
+#'
+#' @export
 
-# Use df name to name csv
 df_name_dot_csv <- function(x) paste0(substitute(x),".csv")
