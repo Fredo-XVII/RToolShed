72935434bd49c542a02ac59aa6ed68e9afeb0d7c
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2020-05-30 22:23:32 -0500

    add example

--- R/geom_mean.R
+++ R/geom_mean.R
@@ -1,5 +1,10 @@
 # Build function for geometric mean
 
+#' @examples
+#' geom_mean_3 <- purrr::partial(geom_mean, roll_window = 3)
+#' geom_mean_roll_3 <- tibbletime::rollify(geom_mean_3,window = 3)
+#'
+#' @export
 geom_mean <- function(x,roll_window) {
   y <- c(x)*1/100 + 1
   z <- prod(c(y))^(1/roll_window) - 1
