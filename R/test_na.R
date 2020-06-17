09016b1ba210e7fd205ea77434a1dc05c32ed410
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2020-06-16 23:39:10 -0500

    if  is.na error

--- R/test_na.R
+++ R/test_na.R
@@ -4,10 +4,11 @@
 test_na <- function(.data,col_n) {
   df_col <- enquo(col_n)
   message(paste(glue::glue("{col_n} has NA's: "), sum(is.na(select(.data,!!df_col))) != 0))
+  na_sum <- is.na(select(.data,!!df_col)) %>% sum()
   testthat::test_that(
     desc = sprintf("Test NA for column: %s", paste0(substitute(.data),'$',col_n)),
     code = testthat::expect_equal(
-      is.na(.data$col_n) %>% sum(), 0
+      na_sum, 0
     )
   )
 }
