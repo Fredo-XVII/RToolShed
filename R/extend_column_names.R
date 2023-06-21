404050767283bd94fccbea7365384e9ee26b0eca
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2023-06-21 11:32:49 -0500

    Update extend_column_names.R
    take advantage of data frame sub-setting which accepts both integers and column names.

--- R/extend_column_names.R
+++ R/extend_column_names.R
@@ -3,18 +3,18 @@
 
 #' @export
 
-ext_col_names <- function(df, text = 'pre', join_cols = c(1), type = c('prefix','suffix')) {
+ext_col_names <- function(df, .text = 'pre', .join_cols = "drvr", .type = c('prefix','suffix')) {
   
   # Separate columns into left right
   df_col_names <- names(df) # All columns
-  col_left <- df_col_names[keep_cols] # No change columns
-  col_right <- df_col_names[-keep_cols] # prefix added columns
+  col_left <- names(df[.join_cols]) # No change columns, takes advantage of df subsetting
+  col_right <- df_col_names[!(df_col_names %in% col_left)] # prefix added columns
   
   # Create right column names based on prefix or suffix
-  if (trimws(type[1]) == 'prefix') {
-    col_right_ordered <- paste(text,col_right, sep = "_")
+  if (trimws(.type[1]) == 'prefix') {
+    col_right_ordered <- paste(.text,col_right, sep = "_")
   } else {
-    col_right_ordered <- paste(col_right,text, sep = "_")
+    col_right_ordered <- paste(col_right,.text, sep = "_")
   }
   
   # combine left and right column name lists'
